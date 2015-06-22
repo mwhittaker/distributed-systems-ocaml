26dcc70e23f334e8d25ce3785d04c606c6d11fcc
Michael Whittaker <mjw297@cornell.edu>
2015-06-22 00:59:14 +0000

    Added comments for choices and pipes.

--- async/choices.ml
+++ async/choices.ml
@@ -1,22 +1,58 @@
+(* Now that we've seen deferreds, return, bind, and map, let's dive into some
+ * more advanced ways of interacting with deferreds. In particular, we'll see
+ * how to wait for a set of deferreds to become determined or wait for one of
+ * many deferreds to become determined. *)
 open Core.Std
 open Async.Std
 
+(* Before we get into choosing from a set of deferreds, let's explore how to
+ * create deferreds that don't become determined for a while. We've seen that
+ * we can use return to create a deferred that is immediately determined. We
+ * can use after and sec to create a deferred that becomes determined after a
+ * while.
+ *
+ * The sec function creates a time span in seconds. For example, `sec 1.`
+ * represents a 1 second time span. The after function takes in a timespan t
+ * and evaluates to a unit deferred that is determined after t has ended.
+ *
+ * This function will print 1 after 1 second, 2 after 2 seconds, and 3 after 3
+ * seconds.
+ *)
 let time () : unit Deferred.t =
-  after (sec 1.) >>= fun () ->
-  printf "%d\n" 1;
-  after (sec 1.) >>= fun () ->
-  printf "%d\n" 2;
-  after (sec 1.) >>| fun () ->
-  printf "%d\n" 3
+  after (sec 1.) >>= fun () -> printf "%d\n" 1;
+  after (sec 1.) >>= fun () -> printf "%d\n" 2;
+  after (sec 1.) >>| fun () -> printf "%d\n" 3
 
+(* We can use after and sec to create a helper function delayed that evaluates
+ * to a deferred which becomes determined with some value after some time. For
+ * example, `delayed 1.2 "hello"` evaluates a deferred that after 1.2 seconds
+ * becomes determined with the string "hello". *)
 let delayed (t: float) (x: 'a) : 'a Deferred.t =
   after (sec t) >>= fun () ->
   return x
 
+(* Now that we can create delayed deferreds with our delayed function, we can
+ * start choosing from a set of deferreds. First up, Deferred.all. Deferred.all
+ * takes in a list of deferreds (i.e. 'a Deferred.t list), and returns a single
+ * list that is deferred (i.e. 'a list Deferred.t). Once every deferred in the
+ * input list becomes determined, the output list also becomes determined, and
+ * it contains all the values of the input list.
+ *
+ * For example, this function creates a list with three deferreds. The first
+ * becomes determined after 0.5 seconds. The second becomes determined after 1
+ * second, and the last becomes determined after 1.5 seconds. After 1.5 seconds
+ * of calling Deferred.all, all three deferreds will have been determined, and
+ * the output list will become determined with the value ["hello"; "world";
+ * "!"]. *)
 let def_all () : unit Deferred.t =
   Deferred.all [delayed 0.5 "hello"; delayed 1. "world"; delayed 1.5 "!"] >>| fun l ->
   List.iter l ~f:(print_endline)
 
+(* Deferred.all_unit is similar to Deferred.all, except it only accepts a list
+ * of unit deferreds, and instead of returning a deferred list of units, it
+ * simple returns a unit deferred. When we run this code, "hello" will print
+ * after 0.5 seconds, "world" will print after 1 second, and "!" will print
+ * after 1.5 seconds. *)
 let def_all_unit () : unit Deferred.t =
   Deferred.all_unit [
     (after (sec 0.5) >>| fun () -> print_endline "hello");
@@ -24,9 +60,39 @@ let def_all_unit () : unit Deferred.t =
     (after (sec 1.5) >>| fun () -> print_endline "!");
   ]
 
+(* Whereas Deferred.all returned a deferred that became determined with *every*
+ * value in a list of deferreds, Deferred.any returns a deferred that is
+ * determined with the *first* value in a list of deferreds that becomes
+ * determined.
+ *
+ * In this code, we create a list of two deferreds. The "fast!" deferred
+ * becomes determined after half a second, and the "slow!" deferred becomes
+ * determined after one second. Since the "fast!" deferred becomes determined
+ * first, the deferred returned by our call to Deferred.any will evaluate to
+ * "fast!". *)
 let def_any () : unit Deferred.t =
   Deferred.any [delayed 0.5 "fast!"; delayed 1.0 "slow!"] >>| print_endline
 
+(* Deferred.all_unit is to Deferred.all as Deferred.any_unit is to
+ * Deferred.any. Expectedly, this code will return a unit deferred that is
+ * determined after 0.5 seconds when the "fast" deferred is determined. Perhaps
+ * unexpectedly, "slow" and "slowest" may still be printed!
+ *
+ * Why is that?
+ *
+ * Here, it's good to remember that >>| and >>= register functions to be called
+ * when certain deferreds become determined. When we evaluate this code, we
+ * first evaluate the list, which contains three elements. Each element is the
+ * evaluation of an invocation to >>|, so all three print_endlines are
+ * registered. Even after 0.5 seconds when "fast" is printed and the deferred
+ * returned by def_any_unit becomes determined, the print_endline "slow" and
+ * print_endline "slowest" functions are still registered and waiting to go. So
+ * depending on how much time remains after this function is called and when
+ * the program terminates, "slow" and "slowest" may or may not be printed!
+ *
+ * You can try experimenting with this by removing the invocation of def_both
+ * in the main function below and see how it affects whether or not "slow" and
+ * "slowest" are printed. *)
 let def_any_unit () =
   Deferred.any_unit [
     (after (sec 0.5) >>| fun () -> print_endline "fast");
@@ -34,6 +100,8 @@ let def_any_unit () =
     (after (sec 1.5) >>| fun () -> print_endline "slowest");
   ]
 
+(* Deferred.both a b acts the same as Deferred.all [a; b] but returns a tuple
+ * instead of a list. *)
 let def_both () : unit Deferred.t =
   Deferred.both (delayed 1. "hello") (delayed 0.5 42) >>| fun (a, b) ->
   printf "%s, %d\n" a b
--- async/pipes.ml
+++ async/pipes.ml
@@ -1,6 +1,21 @@
+(* In this program, we'll explore another core aysnc data structure: the pipe.
+ * Pipes are essentially asynchronous queues. You can write to one end of the
+ * pipe and read from the other end of the pipe. Pipes are great for performing
+ * message passing concurrency and are very similar to channels from the
+ * programming languages like go and rust. *)
 open Core.Std
 open Async.Std
 
+(* The pipe interface is very simple. First, we create a pipe with Pipe.create.
+ * This returns the reader and writer ends of the pipe. Then, we can write to
+ * the writer end of the pipe with Pipe.write and read from the reader end of
+ * the pipe with Pipe.read.
+ *
+ * Try uncommenting the following code and run this program. You'll find the
+ * code never terminates! Turns out Pipe.write returns a deferred that isn't
+ * determined until the pipe is ready to be written to again or until the pipe
+ * is closed. Since, we've sequenced our read after our write, this code hangs
+ * forever. *)
 let write_then_read () : unit Deferred.t =
   (*
   let (r, w) = Pipe.create () in
@@ -11,6 +26,9 @@ let write_then_read () : unit Deferred.t =
   *)
   return ()
 
+(* To fix this problem, we simply don't sequence our read to happen strictly
+ * after our write. We can do so by not waiting for the write using the
+ * don't_wait_for function. Now when we run this code, it'll print "y = 42". *)
 let write_and_read () : unit Deferred.t =
   let (r, w) = Pipe.create () in
   don't_wait_for (Pipe.write w 42);
@@ -18,6 +36,8 @@ let write_and_read () : unit Deferred.t =
   | `Eof  -> failwith "impossible"
   | `Ok y -> printf "y = %d\n" y
 
+(* Alternatively, we can replace `don't_wait_for (Pipe.write w 42)` with
+ * `Pipe.write_without_pushback w 42`*)
 let write_without_pushback_then_read () : unit Deferred.t =
   let (r, w) = Pipe.create () in
   Pipe.write_without_pushback w 42;
@@ -25,19 +45,22 @@ let write_without_pushback_then_read () : unit Deferred.t =
   | `Eof  -> failwith "impossible"
   | `Ok z -> printf "z = %d\n" z
 
+(* Pipes, like lists, can also be folded across. *)
 let pipe_fold () : unit Deferred.t =
-  let (r, w) = Pipe.create () in
-  List.iter (List.range 0 10) ~f:(fun i -> don't_wait_for (Pipe.write w i));
-  Pipe.close w;
+  let r = Pipe.of_list (List.range 0 10) in
   Pipe.fold r ~init:0 ~f:(fun a i -> return (a + i)) >>| fun sum ->
   printf "sum is %d\n" sum
 
+(* Or, itered across. *)
 let pipe_iter () : unit Deferred.t =
-  let (r, w) = Pipe.create () in
-  List.iter (List.range 0 10) ~f:(fun i -> don't_wait_for (Pipe.write w i));
-  Pipe.close w;
+  let r = Pipe.of_list (List.range 0 10) in
   Pipe.iter r ~f:(fun i -> return (printf "read %d\n" i))
 
+(* We can also connect the reader end of one pipe into the writer end of
+ * another pipe using Pipe.transfer. Here, we create two reader/writer pairs
+ * (r, w) and (r', w'). We hook up the first pipe into the second and increment
+ * the values flowing from the first into the second. We then write 42 into the
+ * first pipe and read it out of the second. *)
 let pipe_transfer () : unit Deferred.t =
   let (r, w) = Pipe.create () in
   let (r', w') = Pipe.create () in
@@ -47,6 +70,11 @@ let pipe_transfer () : unit Deferred.t =
   | `Eof  -> failwith "impossible"
   | `Ok a -> printf "a = %d\n" a
 
+(* While we use Pipe.transfer to funnel one pipe into another, we can use
+ * Pipe.interleave to funnel a whole list of pipes into a single pipe. Here, we
+ * create 10 pipes. We write 0 into the zeroth pipe, 1 into the first pipe,
+ * etc. Then we interleave the readers of all these pipes into a single reader
+ * r. Finally, we iterate over `r` and print out the values we see. *)
 let pipe_interleave () : unit Deferred.t =
   let (rs, ws) = List.(unzip (map (range 0 10) ~f:(fun _ -> Pipe.create ()))) in
   List.iteri ws ~f:(fun i w -> don't_wait_for (Pipe.write w i >>| fun () -> Pipe.close w));
