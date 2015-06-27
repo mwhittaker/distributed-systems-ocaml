24765439033916493f33e5cf23a67e9942642a75
Michael Whittaker <mjw297@cornell.edu>
2015-06-26 18:04:14 -0700

    Added more documentation to async chapter.

--- async/moreChoices.ml
+++ async/moreChoices.ml
@@ -1,12 +1,35 @@
+(* Previously in choices.ml, we introduced ourselves to a bunch of very useful
+ * functions that let us choose from a set of deferreds. Now that we've had
+ * more experience, we're going to implement some of these functions! *)
 open Core.Std
 open Async.Std
 
 module D = struct
+  (* First up, both. Recall from earlier that we can think of binds as let
+   * expressions. If we make this mental substitution, we get the following
+   * code:
+   *
+   *   let a = a in
+   *   let b = b in
+   *   (a, b)
+   *
+   * Pretty simple, huh? *)
   let both (a: 'a Deferred.t) (b: 'b Deferred.t) : ('a * 'b) Deferred.t =
    a >>= fun a ->
    b >>= fun b ->
    return (a, b)
 
+  (* Next up, all. Like with both, we can generally ignore the fact that we're
+   * dealing with deferreds and think of the code as if it were full of let
+   * expressions instead of binds.
+   *
+   *   let all ds =
+   *     List.fold_left ds ~init:[] ~f:(fun a d ->
+   *       let a = a in
+   *       let x = d in
+   *       x::a
+   *     ) |> List.rev
+   *)
   let all (ds: 'a Deferred.t list) : 'a list Deferred.t =
     List.fold_left (ds) ~init:(return []) ~f:(fun a d ->
       a >>= fun a ->
@@ -14,6 +37,13 @@ module D = struct
       return (x::a)
     ) >>| List.rev
 
+  (* any is the trickiest of the bunch. If we limit ourselves to using only
+   * binds, we get stuck. Instead, we have to use ivars. First, we read off an
+   * empty deferred from a fresh ivar. Then, we iterate over the list of
+   * deferreds and register them to fill the ivar whenever they become
+   * determined. The deferreds are all racing to fill in the ivar, and the
+   * first one to become determined gets to fill the ivar which in turn
+   * determines the deferred read from it. *)
   let any (ds: 'a Deferred.t list) : 'a Deferred.t =
     let i = Ivar.create () in
     Deferred.List.iter ~how:`Parallel ds ~f:(fun d -> d >>| fun x -> Ivar.fill_if_empty i x)
@@ -21,6 +51,8 @@ module D = struct
     Ivar.read i
 end
 
+(* Here, we repeat the code from choice.ml, but we use our implementations
+ * instead. *)
 let delayed (t: float) (x: 'a) : 'a Deferred.t =
   after (sec t) >>= fun () ->
   return x
--- async/morePipes.ml
+++ async/morePipes.ml
@@ -1,6 +1,33 @@
+(* In this program, we'll use pipes in more advanced and more interesting ways.
+ *)
 open Core.Std
 open Async.Std
 
+(* Imagine we have two pipes: a and b. Someone is writing colors into pipe a
+ * and fruits into pipe b. When we read from pipe a, we get the colors, and
+ * when we read from pipe b, we get the fruits.
+ *
+ *   read a (* blue *)
+ *   read a (* red *)
+ *   read b (* bananas *)
+ *   read b (* strawberries *)
+ *
+ * Now imagine we want to make a single pipe c. Whenever we read from c, we
+ * want to get either a color from a or a fruit from b. That's exactly what the
+ * interleave function does! It takes in a list of readers and returns a single
+ * reader that yields values read from any of the input readers. So if we used
+ * interleave to generate c, here's what the reads might look like.
+ *
+ *   let c = interleave [a; b]
+ *   read c (* blue *)
+ *   read c (* bananas *)
+ *   read c (* strawberries *)
+ *   read c (* red *)
+ *
+ * In terms of implementation, all we do is create a fresh pipe and then
+ * transfer the input readers into it. We also have to make sure to close the
+ * interleaved pipe when all the input pipes are closed.
+ *)
 let interleave (rs: 'a Pipe.Reader.t list) : 'a Pipe.Reader.t =
   let (r, w) = Pipe.create () in
   begin
@@ -9,16 +36,26 @@ let interleave (rs: 'a Pipe.Reader.t list) : 'a Pipe.Reader.t =
   end;
   r
 
+(* Here, we show off how to use interleave. We create 10 pipes and write into
+ * each one. Then we interleave the pipes into a single reader and read all the
+ * values from it. *)
 let pipe_interleave () : unit Deferred.t =
   let (rs, ws) = List.(unzip (map (range 0 10) ~f:(fun _ -> Pipe.create ()))) in
   List.iteri ws ~f:(fun i w -> don't_wait_for (Pipe.write w i >>| fun () -> Pipe.close w));
   let r = interleave rs in
   Pipe.iter r ~f:(fun i -> return (printf "read %d\n" i))
 
+(* With interleave, we took a set of pipes and interleaved them into a single
+ * pipe. Here, we do the exact opposite. We take a single pipe and replicate
+ * its output to a whole set of pipes. We simply iterate over the input pipe
+ * and write the contents to every one of the output pipes. We also make sure
+ * to close all the output pipes once the input pipe has closed. *)
 let broadcast (r: 'a Pipe.Reader.t) (ws: 'a Pipe.Writer.t list) : unit Deferred.t =
   Pipe.iter r ~f:(fun x -> Deferred.all_unit (List.map ws ~f:(fun w -> Pipe.write w x)))
   >>| fun () -> List.iter ws ~f:Pipe.close
 
+(* Here, we show off how to use broadcast. We create a pipe and write "fee",
+ * "fi", "fo", and "fum" into it. Then, we broadcast it into 10 pipes. *)
 let pipe_broadcast () : unit Deferred.t =
   let (r, w) = Pipe.create () in
   let (rs, ws) = List.(unzip (map (range 0 10) ~f:(fun _ -> Pipe.create ()))) in
@@ -31,6 +68,44 @@ let pipe_broadcast () : unit Deferred.t =
   Deferred.List.iteri ~how:`Parallel rs ~f:(fun i r ->
     Pipe.iter r ~f:(fun s -> return (printf "%d: %s\n" i s)))
 
+(* Interleave let a bunch of pipes converge into a single pipe. If we imagine
+ * data flowing through pipes from left to right, interleave looked like this:
+ *   __
+ *     \
+ *      \
+ *   ---->----
+ *      /
+ *   __/
+ *
+ * And broadcast let a single pipe replicate its output to a whole set of
+ * pipes. It looked like this:
+ *          __
+ *         /
+ *        /
+ *   ----<----
+ *        \
+ *         \__
+ *
+ * Now, we'll implement code that let's a whole set of producers (which we'll
+ * call publishers) broadcast data to a whole set of consumers (which we'll
+ * call subscribers). It looks like this:
+ *
+ *   publisher a  __          __ subscriber a
+ *                  \        /
+ *                   \      /
+ *   publisher b  ---->----<---- subscriber b
+ *                   /      \
+ *                __/        \__ subscriber c
+ *   publisher c
+ *
+ * Our publisher/subscriber service will be encapsulate by an instance of type
+ * PubSub.t. Any publisher can publish a value by calling pub. For example, if
+ * I wanted to publish the string "foo" to a pub/sub instance ps, I would call
+ * `PubSub.pub sp "foo"`. Whenever a message is published, it is broadcasted to
+ * all the subscribers. In order to become a subscriber, you call `PubSub.sub`.
+ * This returns the reader end of a pipe from which you can read published
+ * values. There's also a function close which closes all the subscribers'
+ * pipes. *)
 module PubSub = struct
   type 'a t = 'a Pipe.Writer.t list ref
 
@@ -49,6 +124,7 @@ module PubSub = struct
     List.iter !subs ~f:(Pipe.close)
 end
 
+(* Here's a demo of our pub/sub service! *)
 let pipe_pub_sub () : unit Deferred.t =
   let open PubSub in
   let ps = create () in
